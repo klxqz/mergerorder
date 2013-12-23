15f3938b5182f2681da15f43b3159f754ba4fb99
klxqz <klxqz@mail.ru>
2013-12-23 15:26:07 +0600

    next 23.12.2013 15:26

--- lib/actions/shopMergerorderPluginBackendMerge.controller.php
+++ lib/actions/shopMergerorderPluginBackendMerge.controller.php
@@ -5,62 +5,71 @@ class shopMergerorderPluginBackendMergeController extends waJsonController {
     protected $workflow;
 
     public function execute() {
-        $app_settings_model = new waAppSettingsModel();
-        $order_model = new shopOrderModel();
-        $log_model = new shopOrderLogModel();
-        $order_id = waRequest::post('order_id', null, waRequest::TYPE_INT);
-        $order_id_str = shopHelper::encodeOrderId($order_id);
-        $order = $order_model->getOrder($order_id);
 
-        $orders = waRequest::post('orders');
-        $orders_str = array();
-        foreach (array_keys($orders) as $_order_id) {
-            $orders_str[] = shopHelper::encodeOrderId($_order_id);
-        }
+        try {
+            $app_settings_model = new waAppSettingsModel();
+            $order_model = new shopOrderModel();
+            $log_model = new shopOrderLogModel();
+            $order_id = waRequest::post('order_id', null, waRequest::TYPE_INT);
+            $order_id_str = shopHelper::encodeOrderId($order_id);
+            $order = $order_model->getOrder($order_id);
 
-        $merged_orders = implode(', ', $orders_str);
-
-        foreach (array_keys($orders) as $_order_id) {
-            $_order = $order_model->getOrder($_order_id);
-            $items = $_order['items'];
-            foreach ($items as &$item) {
-                unset($item['id']);
-                unset($item['order_id']);
-                $item['price'] = shop_currency($item['price'], $_order['currency'], $order['currency'], false);
+            $orders = waRequest::post('orders');
+            if (!$orders) {
+                $this->errors = 'Необходимо выбрать заказы для объединения';
+                return false;
             }
-            $order['items'] = array_merge($order['items'], $items);
-            $order['discount'] += shop_currency($_order['discount'], $_order['currency'], $order['currency'], false);
+            $orders_str = array();
+            foreach (array_keys($orders) as $_order_id) {
+                $orders_str[] = shopHelper::encodeOrderId($_order_id);
+            }
+
+            $merged_orders = implode(', ', $orders_str);
+
+            foreach (array_keys($orders) as $_order_id) {
+                $_order = $order_model->getOrder($_order_id);
+                $items = $_order['items'];
+                foreach ($items as &$item) {
+                    unset($item['id']);
+                    unset($item['order_id']);
+                    $item['price'] = shop_currency($item['price'], $_order['currency'], $order['currency'], false);
+                }
+                $order['items'] = array_merge($order['items'], $items);
+                $order['discount'] += shop_currency($_order['discount'], $_order['currency'], $order['currency'], false);
+
+                $data = array(
+                    'action_id' => 'comment',
+                    'order_id' => $_order_id,
+                    'before_state_id' => $order['state_id'],
+                    'after_state_id' => 'delete',
+                    'text' => "Удаление после объединения. Заказ $order_id_str объединен с " . $merged_orders
+                );
+                $log_model->add($data);
+                $order_model->delete($_order_id);
+            }
+
+            $order['total'] = $this->calcTotal($order);
+            $order_model->update($order, $order_id);
+
+
+            $log = $log_model->getLog($order_id);
 
             $data = array(
                 'action_id' => 'comment',
-                'order_id' => $_order_id,
+                'order_id' => $order_id,
                 'before_state_id' => $order['state_id'],
-                'after_state_id' => 'delete',
-                'text' => "Удаление после объединения. Заказ $order_id_str объединен с " . $merged_orders
+                'after_state_id' => $order['state_id'],
+                'text' => "Объединение заказов. Заказ $order_id_str объединен с " . $merged_orders
             );
             $log_model->add($data);
-            $order_model->delete($_order_id);
+
+            if ($app_settings_model->get(array('shop', 'mergerorder'), 'email_notification')) {
+                $this->sendNotification($order_id, $merged_orders);
+            }
+            $this->response = '<h1>Заказы успешно объединены</h1>';
+        } catch (Exception $e) {
+            $this->errors = $e->getMessage();
         }
-
-        $order['total'] = $this->calcTotal($order);
-        $order_model->update($order, $order_id);
-
-
-        $log = $log_model->getLog($order_id);
-
-        $data = array(
-            'action_id' => 'comment',
-            'order_id' => $order_id,
-            'before_state_id' => $order['state_id'],
-            'after_state_id' => $order['state_id'],
-            'text' => "Объединение заказов. Заказ $order_id_str объединен с " . $merged_orders
-        );
-        $log_model->add($data);
-
-        if ($app_settings_model->get(array('shop', 'mergerorder'), 'email_notification')) {
-            $this->sendNotification($order_id, $merged_orders);
-        }
-        $this->response = '<h1>Заказы успешно объединены</h1>';
     }
 
     public function calcTotal($data) {
